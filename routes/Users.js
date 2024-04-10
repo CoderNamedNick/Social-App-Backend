d39663ecdf00ab6d4a42b5f34c982fc1b1a34bd8
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-04-10 10:53:28 -0400

    just added birthdate to post

--- routes/Users.js
+++ routes/Users.js
@@ -71,6 +71,7 @@ router.post('/login', async (req, res) => {
       id: user._id,
       username: user.username,
       email: user.email,
+      birthDate: user.birthDate,
       AccDate: user.AccDate,
       guildsJoined: user.guildsJoined,
       parties: user.parties,
