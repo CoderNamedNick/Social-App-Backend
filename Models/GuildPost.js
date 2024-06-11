633c7517d7f17a7e2f265290e62a5c6c7cfeeb4d
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-06-11 10:49:44 -0400

    Update GuildPost.js

--- Models/GuildPost.js
+++ Models/GuildPost.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
-const guildpostSchema = new mongoose.Schema({
-  Guild: { type: mongoose.Schema.Types.ObjectId, ref: 'Guild', required: true },
+const guildpostSchema = new Schema({
+  Guild: { type: Schema.Types.ObjectId, ref: 'Guild', required: true },
   post: [{
-    Poster: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    PosterUserName: {type: String, required: true },
+    Poster: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    PosterUserName: { type: String, required: true },
     content: { type: String, required: true },
     timestamp: { type: Date, default: Date.now },
     Likes: { type: Number, default: 0 },
     Dislikes: { type: Number, default: 0 },
     comments: [{
-      commentingUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-      commentingUserName: {type: String, required: true },
+      commentingUser: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+      commentingUserName: { type: String, required: true },
       commentPost: {
         content: { type: String, required: true },
         timestamp: { type: Date, default: Date.now },
@@ -21,9 +21,10 @@ const guildpostSchema = new mongoose.Schema({
     }]
   }],
   Alerts: [{
-    Poster: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    PosterUserName: {type: String, required: true },
+    Poster: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    PosterUserName: { type: String, required: true },
     content: { type: String, required: true },
+    photo: { type: String }, // URL or reference to the photo
     timestamp: { type: Date, default: Date.now },
     Likes: { type: Number, default: 0 },
     Dislikes: { type: Number, default: 0 },
